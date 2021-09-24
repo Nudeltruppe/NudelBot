c49f396a0060490b3128d48328815144aa343180
MoritzSchittenhelm <78039560+JartC0ding@users.noreply.github.com>
2021-09-24 20:29:04 +0200

    just made a simple hello for testing purposes.

--- src/plugin/plugins/test.ts
+++ src/plugin/plugins/test.ts
@@ -3,16 +3,21 @@ import { Command, CommandEvent, CommandResponse } from "../../command/command";
 import { get_command_manager } from "../../global";
 import { Plugin } from "../plugin";
 
+// Export the plugin as such
 export default {
+    // Define the name and Version
     name: "Test script",
     version: "0.0.1",
 
+    // load in async
     async load() {
-        get_command_manager().add_command(new Command("test", "Just a test", "i will say it once more... ITS A FUCKING TEST", {
+        // Add a command with command tag (hey) command description short and long as well as the code that it executes
+        get_command_manager().add_command(new Command("hey", "Just say Hey!", "#hey to get a hartwarming message!", {
             execute: async (): Promise<CommandResponse> => {
+                // Return a response that gets printed
                 return {
                     is_response: true,
-                    response: "Hello sir!"
+                    response: "You suck lolololol"
                 };
             }
         }, undefined));
